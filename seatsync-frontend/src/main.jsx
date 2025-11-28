90bb6468fd177bfb949793b463e9f85e5bbd87ca
Ranking-254 <pattinnjue@gmail.com>
2025-11-28 09:05:55 +0300

    fixed clerk key

--- seatsync-frontend/src/main.jsx
+++ seatsync-frontend/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 
 // Import key from .env
-const PUBLISHABLE_KEY = import.meta.env.VITE_NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
