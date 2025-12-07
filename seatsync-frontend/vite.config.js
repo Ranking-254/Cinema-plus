e54c12cbb36ef5addcf2365ef71cfaf62535a7a7
Ranking-254 <pattinnjue@gmail.com>
2025-12-08 00:08:59 +0300

    Added PWA support: manifest and offline service worker

--- seatsync-frontend/vite.config.js
+++ seatsync-frontend/vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      devOptions: {
+        enabled: true // <--- This lets you test it on localhost!
+      },
+      manifest: {
+        name: 'Cinema Plus+ Reservations',
+        short_name: 'Cinema+',
+        description: 'Book your seats in real-time.',
+        theme_color: '#E50914',
+        icons: [
+          {
+            src: 'tickets.png', // using this temporary icon for now
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: 'tickets.png',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ],
 })
